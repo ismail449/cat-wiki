cb0ccc384a57d4da9e5b89e31d31affdd041b063
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-11-07 20:37:25 +0200

    added 'Why should you have a cat?' section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import type { InferGetStaticPropsType, GetStaticProps } from "next";
+import Image from "next/image";
+
 import Head from "next/head";
 import CatwikiLogo from "@/components/cat-wiki-logo/cat-wiki-logo";
 import SearchBar from "@/components/search-bar/search-bar";
 import styles from "@/styles/Home.module.css";
 import SearchModal from "@/components/search-modal/search-modal";
-import Card from "@/components/card/card";
 import TopSearchedBreedsSection from "@/components/top-searched-breeds-section/top-searched-breeds-section";
 import SearchResults from "@/components/search-results-list/search-results-list";
 import useBreedSearch from "@/hooks/useBreedSearch";
 import useDidClickOutside from "@/hooks/useDidClickOutside";
 import { getTopTenSearchedBreeds } from "@/lib/cat-breed";
 import { BreedCount } from "@/lib/db/mongoDB";
+import Link from "next/link";
 
 export default function Home({
   topTenSearchedBreeds,
@@ -77,6 +79,36 @@ export default function Home({
               <TopSearchedBreedsSection
                 topSearchedBreeds={topTenSearchedBreeds}
               />
+            </div>
+          </div>
+        </section>
+        <section className={styles.whyShouldOwenCatSection}>
+          <div>
+            <h2 className={styles.title}>Why should you have a cat?</h2>
+            <p className={styles.answer}>
+              Having a cat around you can actually trigger the release of
+              calming chemicals in your body which lower your stress and anxiety
+              levels
+            </p>
+            <Link
+              href="https://www.helpguide.org/articles/healthy-living/joys-of-owning-a-cat.htm"
+              target="_blank"
+            >
+              <span className={styles.readMore}>READ MORE →</span>
+            </Link>
+          </div>
+          <div className={styles.images}>
+            <div className={styles.image1And2Container}>
+              <div className={styles.image2Container}>
+                <Image fill src="/images/image 2.png" alt="sad" />
+              </div>
+              <div className={styles.image1Container}>
+                <Image fill src="/images/image 1.png" alt="sad" />
+              </div>
+            </div>
+
+            <div className={styles.image3Container}>
+              <Image fill src="/images/image 3.png" alt="sad" />
             </div>
           </div>
         </section>
