12f9d9d635b4739defc7ce1bb679e3fd7cf6709a
ismail khaled <esmael449@stud.fci-cu.edu.eg>
2023-11-18 15:52:32 +0200

    added the images section in the breed-details page

--- pages/breed-details/[breedId].tsx
+++ pages/breed-details/[breedId].tsx
@@ -13,75 +13,92 @@ const BreedDetails = ({
   if (typeof breed === "string") {
     return <span>{breed}</span>;
   }
-  const { breedData } = breed;
+  const { breedData, images } = breed;
   return (
     <>
       <Head>
         <title>{breedData.name}</title>
       </Head>
-      <div className={styles.breedDetails}>
-        <div className={styles.imageContainer}>
-          <Image
-            className={styles.image}
-            fill
-            sizes="(max-width: 1024px) 100%"
-            src={breed.images[0].url}
-            alt={breed.breedData.name}
-            priority
-          />
-        </div>
-        <div className={styles.detailsContainer}>
-          <h2 className={styles.breedDetailsTitle}>{breedData.name}</h2>
-          <p className={styles.breedDetailsDescription}>
-            {breedData.description}
-          </p>
-          <p className={styles.breedinfo}>
-            <span>Temperament:</span> {breedData.temperament}{" "}
-          </p>
-          <p className={styles.breedinfo}>
-            <span>Origin:</span> {breedData.origin}{" "}
-          </p>
-          <p className={styles.breedinfo}>
-            <span>Life Span:</span> {breedData.life_span} years
-          </p>
-          <div className={styles.statsContainer}>
-            <span>Adaptability:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.adaptability} />
-            </div>
+      <div className={styles.breedContainer}>
+        <div className={styles.breedDetails}>
+          <div className={styles.imageContainer}>
+            <Image
+              className={styles.image}
+              fill
+              sizes="(max-width: 1024px) 100%"
+              src={images[0].url}
+              alt={breedData.name}
+              priority
+            />
+          </div>
+          <div className={styles.detailsContainer}>
+            <h2 className={styles.breedDetailsTitle}>{breedData.name}</h2>
+            <p className={styles.breedDetailsDescription}>
+              {breedData.description}
+            </p>
+            <p className={styles.breedinfo}>
+              <span>Temperament:</span> {breedData.temperament}{" "}
+            </p>
+            <p className={styles.breedinfo}>
+              <span>Origin:</span> {breedData.origin}{" "}
+            </p>
+            <p className={styles.breedinfo}>
+              <span>Life Span:</span> {breedData.life_span} years
+            </p>
+            <div className={styles.statsContainer}>
+              <span>Adaptability:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.adaptability} />
+              </div>
 
-            <span>Affection level:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.affection_level} />
-            </div>
-            <span>Child Friendly:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.child_friendly} />
-            </div>
-            <span>Grooming:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.grooming} />
-            </div>
-            <span>Intelligence:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.intelligence} />
-            </div>
+              <span>Affection level:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.affection_level} />
+              </div>
+              <span>Child Friendly:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.child_friendly} />
+              </div>
+              <span>Grooming:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.grooming} />
+              </div>
+              <span>Intelligence:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.intelligence} />
+              </div>
 
-            <span>Health issues:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.health_issues} />
-            </div>
+              <span>Health issues:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.health_issues} />
+              </div>
 
-            <span>Social needs:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.social_needs} />
-            </div>
+              <span>Social needs:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.social_needs} />
+              </div>
 
-            <span>Stranger friendly:</span>
-            <div>
-              <Level maxLevel={5} level={breedData.stranger_friendly} />
+              <span>Stranger friendly:</span>
+              <div>
+                <Level maxLevel={5} level={breedData.stranger_friendly} />
+              </div>
             </div>
           </div>
+        </div>
+        <h2 className={styles.breedImagesTitle}>Other photos</h2>
+        <div className={styles.breedImages}>
+          {images.map(({ url, id }, index) => {
+            return (
+              <div className={styles.imageItemContainer} key={id}>
+                <Image
+                  className={styles.imageItem}
+                  src={url}
+                  fill
+                  alt={`${breedData.name}-${index}`}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
